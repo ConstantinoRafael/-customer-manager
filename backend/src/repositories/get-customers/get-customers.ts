4e4010d17a59b0e546057356fbfb441e47058dff
Rafael <rafael.constantino89@gmail.com>
2024-03-09 03:36:58 -0300

    backend-feat: config pg database

--- backend/src/repositories/get-customers/get-customers.ts
+++ backend/src/repositories/get-customers/get-customers.ts
@@ -1,14 +1,10 @@
 import { IGetCustomersRepository } from "../../controllers/get-customers/protocols";
+import { connection } from "../../database/database";
 import { Customer } from "../../models/customer";
 
 export class GetCustomersRepository implements IGetCustomersRepository {
   async getCustomers(): Promise<Customer[]> {
-    return [
-      {
-        name: "Rafael",
-        email: "rafael@rafael.com",
-        phone: "111111111",
-      },
-    ];
+    const customers = await connection.query("SELECT * FROM customers");
+    return customers.rows;
   }
 }
